e6bda4246442da2e2f9a08b6cb68b103684429ab
Jawad Khizran <jawad.khizran11@gmail.com>
2024-12-13 18:19:28 +0200

    Remove unnecessary 'sudo'

--- Deployment/user_data.sh
+++ Deployment/user_data.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# Install git and clone files to the Web Server 
-sudo yum update -y   # For Amazon Linux or CentOS
-sudo yum install -y git
-cd 
+yum update -y   # For Amazon Linux or CentOS
+
+# Install git and clone files to the Web Server
+yum install -y git
+cd
 mkdir App
-sudo mkdir .aws
+mkdir .aws
 cd App
 git clone https://github.com/roee4643/MemeGenerator
 cd ..
 
 # Ensure that pip is installed and is the latest version
-sudo python3 -m ensurepip --upgrade
-sudo python3 -m pip install --upgrade pip
+python3 -m ensurepip --upgrade
+python3 -m pip install --upgrade pip
 
 # Install and run docker
 cd App
 
-sudo yum install docker -y
-sudo service docker start
+yum install docker -y
+service docker start
 
-sudo docker build -t meme-api .
-sudo docker run -p 5000:5000 meme-api
+docker build -t meme-api .
+docker run -p 5000:5000 meme-api
